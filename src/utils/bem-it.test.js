8b41730a4b203ec6da9ac9a74ee91e2b5983448b
Mark Graham <markgr26@googlemail.com>
2021-01-28 15:59:07 +0000

    Initial commit.

---
+++ src/utils/bem-it.test.js
@@ -0,0 +1,44 @@
+import BemIt from './bem-it';
+
+test('Outputs a block', () => {
+  const blockString = 'Block';
+  const bem = new BemIt(blockString);
+  expect(bem.val).toEqual(blockString);
+});
+
+test('Outputs an element', () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  const element = 'Element';
+  expect(bem.el(element).val).toEqual(
+    `${block}__${element}`);
+});
+
+test('Outputs a modifier', () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  const element = 'Element';
+  const modifier = 'modifier';
+  expect(bem.el(element).mod(modifier)).toEqual(
+    `${block}__${element} ${block}__${element}--${modifier}`);
+});
+
+test('Outputs multiple modifiers', () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  const element = 'Element';
+  const modifier1 = 'modifier1';
+  const modifier2 = 'modifier2';
+  expect(bem.el(element).mod([modifier1, modifier2])).toEqual(
+    `${block}__${element} ${block}__${element}--${modifier1} ${block}__${element}--${modifier2}`);
+});
+
+test('Clear after val', () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  const element = 'Element';
+  const val = bem.el(element).val;
+  const element2  = 'Element2'
+  expect(bem.el(element2).val).toEqual(
+    `${block}__${element2}`);
+});
---
+++ src/utils/bem-it.ts
@@ -0,0 +1,34 @@
+export default class BemIt {
+
+  private _block: string;
+  private _value: string;
+
+  constructor(block: string) {
+    this._block = block;
+    this._value = block;
+  }
+
+  public el(val: string): BemIt {
+    this._value = `${this._value}__${val}`;
+    return this;
+  }
+
+  public mod(val: string | string[]): string {
+    const fullMod = (v: string): string => {
+      return `${this._value}${v ? `--${v}` : '' }`;
+    }
+
+    const ret = `${this._value} ${Array.isArray(val)
+      ? val.map(m => fullMod(m)).join(' ')
+      : `${fullMod(val)}`}`;
+    
+    this._value = this._block;
+    return ret;
+  }
+
+  public get val(): string {
+    const ret = this._value;
+    this._value = this._block;
+    return ret;
+  }
+}
