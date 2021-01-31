827b6b8b5b3507978220c0a47643b4aafcf78ba9
Mark Graham <markgr26@googlemail.com>
2021-01-31 22:05:11 +0000

    Refactored BemIt with breaking changes. Improved test suite.

--- src/utils/bem-it.test.js
+++ src/utils/bem-it.test.js
@@ -1,52 +1,180 @@
 import BemIt from './bem-it';
 
-test('Outputs a block', () => {
+test('Output a block.', () => {
   const blockString = 'Block';
   const bem = new BemIt(blockString);
-  expect(bem.val).toEqual(blockString);
+  expect(bem.out).toEqual(blockString);
 });
 
-test('Outputs an element', () => {
-  const block = 'Block';
+test('Output an element.', () => {
+  const
+  block = 'Block',
+  element = 'element';
+
   const bem = new BemIt(block);
-  const element = 'Element';
-  expect(bem.el(element).val).toEqual(
+
+  expect(bem.el(element).out).toEqual(
     `${block}__${element}`);
 });
 
-test('Outputs a modifier', () => {
-  const block = 'Block';
+test('Output an element with modifier.', () => {
+  const
+  block = 'Block',
+  element = 'element',
+  modifier = 'modifier';
+
   const bem = new BemIt(block);
-  const element = 'Element';
-  const modifier = 'modifier';
-  expect(bem.el(element).mod(modifier)).toEqual(
+
+  expect(bem.el(element).mod(modifier).out).toEqual(
     `${block}__${element} ${block}__${element}--${modifier}`);
 });
 
-test('Outputs multiple modifiers', () => {
-  const block = 'Block';
+test('Empty string modifier ignored.', () => {
+  const
+  block = 'Block',
+  element = 'element',
+  modifier = '';
+
   const bem = new BemIt(block);
-  const element = 'Element';
-  const modifier1 = 'modifier1';
-  const modifier2 = 'modifier2';
-  expect(bem.el(element).mod([modifier1, modifier2])).toEqual(
-    `${block}__${element} ${block}__${element}--${modifier1} ${block}__${element}--${modifier2}`);
+
+  expect(bem.el(element).mod(modifier).out).toEqual(
+    `${block}__${element}`);
 });
 
-test('Empty modifier', () => {
-  const block = 'Block';
+test('Space string modifier ignored.', () => {
+  const
+  block = 'Block',
+  element = 'element',
+  modifier = ' ';
+
   const bem = new BemIt(block);
-  const element = 'Element';
-  const modifier = '';
-  expect(bem.el(element).mod(modifier)).toEqual(`${block}__${element}`);
+
+  expect(bem.el(element).mod(modifier).out).toEqual(
+    `${block}__${element}`);
 });
 
-test('Clear after val', () => {
-  const block = 'Block';
+test('Multiple elements.', () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2';
+
   const bem = new BemIt(block);
-  const element = 'Element';
-  const val = bem.el(element).val;
-  const element2  = 'Element2'
-  expect(bem.el(element2).val).toEqual(
+
+  expect(bem.el(element1).el(element2).out).toEqual(
+    `${block}__${element1}__${element2}`);
+});
+
+test('Multiple elements with modifier.', () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2',
+  modifier = 'modifier';
+
+  const bem = new BemIt(block);
+
+  expect(bem.el(element1).el(element2).mod(modifier).out).toEqual(
+    `${block}__${element1}__${element2} ${block}__${element1}__${element2}--${modifier}`);
+});
+
+test(`Calling 'out' resets value back to block.`, () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2';
+
+  const bem = new BemIt(block);
+  bem.el(element1).out;
+
+  expect(bem.el(element2).out).toEqual(
     `${block}__${element2}`);
+});
+
+test(`Chaining bems.`, () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2',
+  modifier = 'modifier';
+
+  const bem = new BemIt(block);
+
+  expect(bem.el(element1).mod(modifier).and.el(element2).out).toEqual(
+    `${block}__${element1} ${block}__${element1}--${modifier} ${block}__${element2}`);
+});
+
+test(`An unchained variation.`, () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2',
+  modifier = 'modifier';
+
+  const bem = new BemIt(block);
+  bem.el(element1);
+  bem.el(element2);
+  bem.mod(modifier);
+  const out = bem.out;
+
+  expect(out).toEqual(
+    `${block}__${element1}__${element2} ${block}__${element1}__${element2}--${modifier}`);
+});
+
+test(`Component simulation.`, () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2',
+  modifier = 'modifier';
+
+  const bem = new BemIt(block);
+
+  expect(bem.out).toEqual(
+    `${block}`);
+
+  expect(bem.el(element1).out).toEqual(
+    `${block}__${element1}`);
+
+  expect(bem.el(element2).mod(modifier).out).toEqual(
+    `${block}__${element2} ${block}__${element2}--${modifier}`);
+});
+
+test(`A ridiculous one that I hope you never use.`, () => {
+  const
+  block = 'Block',
+  element1 = 'element1',
+  element2  = 'element2',
+  element3  = 'element3',
+  modifier1 = 'modifier1',
+  modifier2 = 'modifier2';
+
+  const bem = new BemIt(block);
+
+  expect(bem.el(element1).el(element2).mod(modifier1).and.el(element3).mod(modifier2).out).toEqual(
+    `${block}__${element1}__${element2} ${block}__${element1}__${element2}--${modifier1} ${block}__${element3} ${block}__${element3}--${modifier2}`);
+});
+
+test(`Edge case, empty strings.`, () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  expect(bem.el('').el('').mod('').out).toEqual(`${block}`);
+});
+
+test(`Edge case, undefined.`, () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  expect(bem.el(undefined).el(undefined).mod(undefined).out).toEqual(`${block}`);
+});
+
+test(`Edge case, null.`, () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  expect(bem.el(null).el(null).mod(null).out).toEqual(`${block}`);
+});
+
+test(`Edge case, mixed.`, () => {
+  const block = 'Block';
+  const bem = new BemIt(block);
+  expect(bem.el('').el(null).mod(undefined).out).toEqual(`${block}`);
 });
--- src/utils/bem-it.ts
+++ src/utils/bem-it.ts
@@ -1,36 +1,64 @@
 export default class BemIt {
 
-  private _block: string;
-  private _value: string;
+  private _block: string = '';
+  private _lastBlockObj: BemIt | null = null;
+  private _fullBem: string = '';
 
-  constructor(block: string) {
-    this._block = block;
-    this._value = block;
+  constructor(block: string | BemIt) {
+    if (typeof block === 'string') {
+      this._block = block;
+      this._fullBem = block;
+    } else {
+      this._lastBlockObj = block;
+      this._fullBem = block.blockString;
+    }
   }
 
-  public el(val: string): BemIt {
-    this._value = `${this._value}__${val}`;
+  get blockString(): string {
+    if (this._block) {
+      return this._block;
+    }
+    return this._lastBlockObj?.blockString || '';
+  }
+
+  get peek(): string {
+    return this._fullBem;
+  }
+
+  get out(): string {
+    const ret = this._block
+      ? this._fullBem
+      : `${this._lastBlockObj?.out} ${this._fullBem}`;
+    this._fullBem = this.blockString;
+    this._lastBlockObj = null;
+    return ret;
+  }
+
+  get and(): BemIt {
+    return new BemIt(this);
+  }
+
+  public el(element: string | undefined | null): BemIt {
+    this._fullBem = element
+      ? `${this._fullBem}__${element.trim()}`
+      : this._fullBem;
+
     return this;
   }
 
-  public mod(val: string | string[]): string {
-    if (val) {
-      const fullMod = (v: string): string => {
-        return `${this._value}${v ? `--${v}` : '' }`;
-      }      
-      const ret = `${this._value} ${Array.isArray(val)
-        ? val.map(m => fullMod(m)).join(' ')
-        : `${fullMod(val)}`}`;
-      
-      this._value = this._block;
-      return ret;
+  public mod(modifier: string | undefined | null): any {
+    modifier = modifier?.trim();
+    if (modifier) {
+      this._fullBem = `${this._fullBem} ${this._fullBem}${`--${modifier}`}`;
     }
-    return this._value;
-  }
-
-  public get val(): string {
-    const ret = this._value;
-    this._value = this._block;
-    return ret;
+    return {
+      lastBem: this,
+      get and(): BemIt {
+        return new BemIt(this.lastBem);
+      },
+      get out(): string {
+        return this.lastBem.out;
+      }      
+    }
   }
 }
